da4070456c7cc3f4e565b465bce078dbc8291ded
TheJoker1985 <the_adicts@hotmail.com>
2025-08-15 06:03:50 -0400

    🔧 Fix: Correction import os dans settings.py - Application testée et fonctionnelle

--- markev_project/settings.py
+++ markev_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ztc5w7=a(wjnzac4+2ng#7q=dzoa(7t2)takvl0xj3!r^iqokr')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-import os
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['*']  # Vercel handles domain restrictions
