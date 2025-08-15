4a14bade2eaf254395869a355d30930f42b9b62d
TheJoker1985 <the_adicts@hotmail.com>
2025-08-15 07:55:15 -0400

    fix: Add app alias for Vercel in wsgi.py

--- markev_project/wsgi.py
+++ markev_project/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'markev_project.settings')
 
 application = get_wsgi_application()
+
+# Alias pour Vercel
+app = application
